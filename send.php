b232b061265d373f05d4a9618e32fba6188bf35c
QuyenWibu <91710203+QuyenWibu@users.noreply.github.com>
2023-10-15 22:17:27 +0700

    fff

--- send.php
+++ send.php
@@ -1,5 +1,6 @@
 <?php
 use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
 use PHPMailer\PHPMailer\Exception;
 
 require 'PHPMailer/src/Exception.php';
@@ -7,15 +8,14 @@ require 'PHPMailer/src/PHPMailer.php';
 require 'PHPMailer/src/SMTP.php';
 
 if(isset($_POST["submit"])){
-    $mail = new PHPMailer(true);
-
+    $mail = new PHPMailer(true);  
     $mail->isSMTP();
     $mail->Host = 'smtp.gmail.com';
     $mail->SMTPAuth = true;
     $mail->Username = 'aakjaz@gmail.com';
-    $mail->Password = 'dzmc dnvk plbs ycij';
+    $mail->Password = 'sumipdehowgpkif';
     $mail->SMTPSecure = 'ssl';
-    $mail->Port = '456';
+    $mail->Port = 465;
 
     $mail->setFrom('aakjaz@gmail.com');
 
@@ -29,13 +29,5 @@ if(isset($_POST["submit"])){
 
     $mail->submit();
 
-
-    echo 
-    "
-    <script>
-    alert('sent Successfully');
-    document.location.href = 'index.html';
-    </script>
-    "
 }
 ?>
