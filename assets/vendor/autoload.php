8d0459025c7e2d79ff7421f004a11471a4dde880
QuyenWibu <91710203+QuyenWibu@users.noreply.github.com>
2023-10-15 15:13:43 +0700

    ddd

---
+++ assets/vendor/autoload.php
@@ -0,0 +1,7 @@
+<?php
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+
+require 'path/to/PHPMailer/src/Exception.php';
+require 'path/to/PHPMailer/src/PHPMailer.php';
+require 'path/to/PHPMailer/src/SMTP.php';
--- assets/vendor/php-email-form/php-email-form.php
+++ assets/vendor/php-email-form/php-email-form.php
@@ -1,44 +1,35 @@
 <?php
 
-class PHP_Email_Form {
-  public $to;
-  public $from_name;
-  public $from_email;
-  public $subject;
-  public $message;
-  public $headers;
-  public $smtp;
+$name = $_POST["name"];
+$email = $_POST["email"];
+$subject = $_POST["subject"];
+$message = $_POST["message"];
 
-  public function send() {
-    if (empty($this->to) || empty($this->from_name) || empty($this->from_email) || empty($this->subject) || empty($this->message)) {
-      return false;
-    }
+require "vendor/autoload.php";
 
-    $this->headers = "From: $this->from_name <$this->from_email>\r\nReply-To: $this->from_email\r\n";
-    
-    if (!empty($this->smtp)) {
-      $this->headers .= "MIME-Version: 1.0\r\n";
-      $this->headers .= "Content-type: text/html; charset=utf-8\r\n";
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
 
-      ini_set("SMTP", $this->smtp['host']);
-      ini_set("smtp_port", $this->smtp['port']);
-      ini_set("sendmail_from", $this->from_email);
-      ini_set("auth_username", $this->smtp['username']);
-      ini_set("auth_password", $this->smtp['password']);
-    }
-    
-    return mail($this->to, $this->subject, $this->message, $this->headers);
-  }
+$mail = new PHPMailer(true);
 
-  public function add_message($content, $label = '', $line_breaks = 1) {
-    $content = strip_tags($content);
-    
-    if (!empty($label)) {
-      $this->message .= "<strong>$label:</strong> ";
-    }
-    
-    $this->message .= nl2br($content) . str_repeat("<br>", $line_breaks);
-  }
-}
+// $mail->SMTPDebug = SMTP::DEBUG_SERVER;
 
-?>
+$mail->isSMTP();
+$mail->SMTPAuth = true;
+
+$mail->Host = "smtp.example.com";
+$mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
+$mail->Port = 587;
+
+$mail->Username = "you@example.com";
+$mail->Password = "password";
+
+$mail->setFrom($email, $name);
+$mail->addAddress("dave@example.com", "Dave");
+
+$mail->Subject = $subject;
+$mail->Body = $message;
+
+$mail->send();
+
+header("Location: sent.html");
